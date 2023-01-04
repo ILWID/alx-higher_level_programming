a9448d9abd8a47847913233fe7aa052b46290011
ILWID <Obasanyaolufemi@gmail.com>
2023-01-03 19:17:48 -0800

    initial commit update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-FIZZ = "Fizz"
-BUZZ = "Buzz"
-
-
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 and number % 5):
-            print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
-            print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
-            print("%s" % (BUZZ), end=' ')
+    for var in range(1, 101):
+        if var % 3 == 0 and var % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif var % 3 == 0:
+            print("Fizz", end=' ')
+        elif var % 5 == 0:
+            print("Buzz", end=' ')
         else:
-            print("%d" % (number), end=' ')
+            print(f"{var:d}", end=' ')
