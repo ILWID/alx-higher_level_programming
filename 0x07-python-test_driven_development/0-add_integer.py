f4f33de0f9c8a48f460948ab6813176a32a547de
ILWID <Obasanyaolufemi@gmail.com>
2023-02-03 00:56:34 -0800

    error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""
-    This module contains a function add_integer(a, b)
-    that adds two integers or floats and return the result,
-    a and b must be integers or floats otherwise a
-    TypeError is raised.
-    If a or b is a float, it is first casted to an int so
-    the funtion always returns an integer as the result
-"""
+"""contains a function to add two integers"""
 
 
 def add_integer(a, b=98):
-    """ Adds two integers and returns the result """
-
-    if type(a) is not int and type(a) is not float:
+    """function that returs an integer
+    args:
+        a (int or float): first param
+        b (int or float): second param
+    Raises TypeError with corresponding message if a or b
+    isn't an int or a float
+    """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("a must be an integer")
-
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-
-    return a + b
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
